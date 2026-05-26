1c289562292e3a79c8aee3aca1709bc688bc2261
YO0RSH <jorgerchiguay@gmail.com>
2026-05-25 22:20:07 -0400

    funcion que cuenta dias
    Add function to count days and manage file creation.

--- datos.py
+++ datos.py
@@ -1,1 +1,12 @@
-
+def conteo_dias():
+    try: #en este bloque de try creamos el archivo con el dia uno por defecto
+        with open('dias.txt','x') as archivo:# con x creamos y da error cuando el archivo ya existe
+            pass
+        with open('dias.txt','w') as archivo:
+            archivo.write(str(1))
+    except FileExistsError:# entonces cuando da error por que el archivo ya existe, se ejecuta esto
+        with open('dias.txt','r') as archivo:
+            dia_actual = int(archivo.read())
+        with open('dias.txt','w') as archivo:
+            nuevo_dia = dia_actual + 1
+            archivo.write(str(nuevo_dia))
